a8235d4101f180334ef67067292670bc6c01a945
Rahul Shukla <shukla2009@gmail.com>
2021-05-30 18:42:13 +0800

    Structure Format

---
+++ Binary_Tree_Inorder_Traversal/test.py
@@ -0,0 +1,12 @@
+from solution import Solution
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+node3 = TreeNode(3)
+node2 = TreeNode(2,node3)
+node1 = TreeNode(1,None,node2)
+assert [1,3,2] == Solution().inorderTraversal(node1) , 'Test Case Failed'
